f1030f7435e791590117161306750b3c2999164d
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-09-25 22:24:11 +0200

    Add function that handles the submition of the form

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -13,6 +13,22 @@ function Education({ educationList, setEducationList }) {
   function handleChange(e) {
     setForm({ ...form, [e.target.name]: e.target.value });
   }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (isEditing) {
+      const updatedList = [...educationList];
+      updatedList[currentEditIndex] = form;
+      setEducationList(updatedList);
+      setIsEditing(false);
+      setCurrentEditIndex(null);
+    } else {
+      setEducationList([...educationList, form]);
+    }
+
+    setForm({ school: '', degree: '', year: '' });
+  }
 }
 
 export { Education };
